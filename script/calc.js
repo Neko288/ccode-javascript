bfcf7a145375c78cb6bae0089a79a159ef6dfe8a
Neko288 <ryzen.kuro@outlook.jp>
2021-11-23 00:38:17 +0900

    javascriptを更新。まだ、暗号化が使えない

--- script/calc.js
+++ script/calc.js
@@ -1,25 +1,27 @@
-let key;
-let detext;
-let entext;
-
+let key='';
+let detext='';
+let entext='';
 let result = '';
-let deresult = '';
+let deresult='';
+let unic = '';
+let list_text ='';
 
-
-document.getElementById("GOen").addEventListener("click", encode);
-document.getElementById("GOde").addEventListener("click", encode);
+document.getElementById("GOen").addEventListener("click", encode());
+document.getElementById("GOde").addEventListener("click", decode());
 
 
 function encode() {
   key = document.getElementById('key').value;
   entext = document.getElementById('entext').value;
-  let list_text = Array.from(entext);
+  list_text = Array.from(entext);
 
   for (var i=0;i<list_text.length;i++) {
-    result += (entext.codePointAt(i))*key+','
+    unic = (entext.codePointAt(i))+key;
+    result+=String.fromCodePoint(unic);
   }
   
   document.getElementById("value").value = result;
+  list_text = ''
   result = ' '
   entext = ' '
   key = ' '
@@ -30,14 +32,17 @@ function encode() {
 function decode() {
   key = document.getElementById('key').value;
   detext = document.getElementById('detext').value;
+  list_text = Array.from(detext);
 
-  for (var i=0;i<detext.length;i++) {
-    deresult += (String.fromCodePoint(detext[i]/key))
+  for (var i=0;i<list_text.length;i++) {
+    unic = (detext.codePointAt(i))-key;
+    deresult+=String.fromCodePoint(unic);
   }
 
   document.getElementById("value").value = deresult;
+  list_text = ''
   deresult = ' '
-  entext = ' '
+  detext = ' '
   key = ' '
   form.reset();
 }
