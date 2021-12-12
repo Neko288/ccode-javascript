a010822051fe0a4b0160e4de496a1a0a10c025ac
Neko288 <ryzen.kuro@outlook.jp>
2021-12-13 05:31:59 +0900

    test導入PWS

---
+++ script/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'ccode-cache';
+const urlsToCache = [
+  './',
+  '../index.html',
+  '../style.css',
+  'calc.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
